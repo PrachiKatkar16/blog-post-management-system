f634972131ba76c479190a5f1c0446c2724ec5fa
PrachiKatkar16 <prachikatkar116@gmail.com>
2026-06-03 20:45:07 +0530

    cors updated

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,7 +5,11 @@ const cors = require('cors');
 const app=express()
 
 app.use(express.json())
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use("/api/posts", postRoutes);
 
