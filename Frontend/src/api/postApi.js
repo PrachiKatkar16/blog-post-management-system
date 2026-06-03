971bb88964ab3c8317b27b87e6db808d1d833131
PrachiKatkar16 <prachikatkar116@gmail.com>
2026-06-03 22:10:37 +0530

    changes made in postApi

--- Frontend/src/api/postApi.js
+++ Frontend/src/api/postApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/posts",
+  baseURL: "https://blog-post-management-system-uoz5.onrender.com",
 });
 
 export const getPosts = (page = 1, limit = 5) => {
