1e5044a129160be85d3c2b7570699f634eb27b92
PrachiKatkar16 <prachikatkar116@gmail.com>
2026-06-03 23:17:54 +0530

    cors updated

--- Frontend/src/api/postApi.js
+++ Frontend/src/api/postApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://blog-post-management-system-2.onrender.com/api/posts",
+  baseURL: "https://blog-post-management-system-4.onrender.com/api/posts",
 });
 
 export const getPosts = (page = 1, limit = 5) => {
